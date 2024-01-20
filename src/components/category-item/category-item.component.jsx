27f6e53558d9b6becb2e6d67a353b2770f18206b
Cesar Marrujo <cesarmarrujo@Cesars-MBP.lan>
2024-01-19 17:25:45 -0800

    Initial layout structure and styles

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,17 @@
+import './category-item.styles.scss';
+
+const CategoryItem = ({category}) => {
+  const { imageUrl, title } = category;
+
+  return (
+    <div className='category-container' style={{backgroundImage: `url(${imageUrl})`}}>
+      <div className="category-container-background"></div>
+      <div className="category-body-container">
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </div>
+    </div> 
+  )
+}
+
+export default CategoryItem;
